5683951411fccb860f087774ca9281f995416fd1
Alex <alexkrzeminsky@gmail.com>
2020-01-27 22:05:54 +0100

    cmake

---
+++ cmake/CMakeFiles/Shell.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alex/Desktop/UXP1A/UxpShell/Access.cpp" "/home/alex/Desktop/UXP1A/UxpShell/cmake/CMakeFiles/Shell.dir/Access.cpp.o"
+  "/home/alex/Desktop/UXP1A/UxpShell/PipelineContext.cpp" "/home/alex/Desktop/UXP1A/UxpShell/cmake/CMakeFiles/Shell.dir/PipelineContext.cpp.o"
+  "/home/alex/Desktop/UXP1A/UxpShell/Routine.cpp" "/home/alex/Desktop/UXP1A/UxpShell/cmake/CMakeFiles/Shell.dir/Routine.cpp.o"
+  "/home/alex/Desktop/UXP1A/UxpShell/Shell.cpp" "/home/alex/Desktop/UXP1A/UxpShell/cmake/CMakeFiles/Shell.dir/Shell.cpp.o"
+  "/home/alex/Desktop/UXP1A/UxpShell/Streams.cpp" "/home/alex/Desktop/UXP1A/UxpShell/cmake/CMakeFiles/Shell.dir/Streams.cpp.o"
+  "/home/alex/Desktop/UXP1A/UxpShell/echo.cpp" "/home/alex/Desktop/UXP1A/UxpShell/cmake/CMakeFiles/Shell.dir/echo.cpp.o"
+  "/home/alex/Desktop/UXP1A/UxpShell/main.cpp" "/home/alex/Desktop/UXP1A/UxpShell/cmake/CMakeFiles/Shell.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake/CMakeFiles/Shell.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shell.dir/Access.cpp.o"
+  "CMakeFiles/Shell.dir/PipelineContext.cpp.o"
+  "CMakeFiles/Shell.dir/Routine.cpp.o"
+  "CMakeFiles/Shell.dir/Shell.cpp.o"
+  "CMakeFiles/Shell.dir/Streams.cpp.o"
+  "CMakeFiles/Shell.dir/echo.cpp.o"
+  "CMakeFiles/Shell.dir/main.cpp.o"
+  "builds/Shell.pdb"
+  "builds/Shell"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
